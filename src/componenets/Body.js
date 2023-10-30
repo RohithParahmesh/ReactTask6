1ba60fe31cda896101336485f5c22bac98f00165
Rohith Parahmesh <rohithparahmesh@gmail.com>
2023-10-30 23:41:37 +0530

    Update Body.js

--- src/componenets/Body.js
+++ src/componenets/Body.js
@@ -23,27 +23,27 @@ export function Body() {
         </svg>
       </div>
       <div className="bg-svg-blue p-5 text-center text-light">
-        <h1>Varun Sudhir</h1>
+        <h1>RohithParahmesh</h1>
         <div className="h4">Vellore Institute of Technology</div>
         <div className="h4">
-          Senior Core Member at IEEE VIT Vellore
+          Senior Core Member at IEEE VIT Vellore, Intern at Ford motor company
         </div>
         <div className="h4">
-          Email: <a href="/#">varun.sudhir2021@vitstudent.ac.in</a>
+          Email: <a href="/#">rohithparahmesh.k2021@vitstudent.ac.in</a>
         </div>
         <div className="h4">
           LinkedIn:{" "}
           <a
-            href="https://www.linkedin.com/in/varun-sudhir-b097ab228/"
+            href="https://www.linkedin.com/in/rohith-parahmesh-k-984855225/"
             target="blank"
           >
-            https://www.linkedin.com/in/varun-sudhir-b097ab228/
+            hhttps://www.linkedin.com/in/rohith-parahmesh-k-984855225/
           </a>
         </div>
         <div className="h4">
           GitHub:{" "}
-          <a href="https://github.com/varun10sudhir" target="blank">
-          https://github.com/varun10sudhir
+          <a href="https://github.com/RohithParahmesh" target="blank">
+          https://github.com/RohithParahmesh
           </a>
         </div>
       </div>
@@ -69,12 +69,7 @@ export function Body() {
         <div className="text-center text-light px-5">
           <h1>About</h1>
           <div>
-          I'm Varun Sudhir, a committed and forward-thinking professional deeply passionate about both data science and web development. 
-          I'm eager to contribute my expertise to dynamic teams and innovative projects. 
-          My academic journey at VIT, where I achieved a CGPA of 8.93, provided me with in-depth knowledge in networking and database management, thanks to the meticulously planned course curriculum.
-          Throughout my three years of exploration and learning, I have refined my programming skills in languages like Java, C++, Python, and C. 
-          As a student, my fervor lies in web development, machine learning, and deep learning. 
-          I have been diligently working to expand my expertise in these areas, driven by my passion for creating innovative digital solutions.
+          I'm Rohith Parahmesh,An inquisitive and sincere individual, driven by ambition, with an innate knack for all things Tech, and a delightful penchant for dabbling in the world of business. With a heart brimming with curiosity and a mind determined to carve new pathways, I aspire to unveil innovative data solutions that not only simplify, but truly enrich lives.🌟
           </div>
         </div>
       </div>
@@ -108,13 +103,22 @@ export function Body() {
               <div>May 2023 - Present(On-site)</div>
               <p>
                 During my tenure at IEEE, I have learnt to apply my skills in
-                Web Development and Marketing domains and have successfully
+                machine learning and sponsorship domains and have successfully
                 assisted various participants who attended the events
                 that was held in VIT by our chapter.
               </p>
               <p>
-                Skills: Python (Programming Language) · Flask·
-                NumPy · Express and Node
+              SKILLS
+              Python , C++, SQL
+              EDA, ETL, Feature engineering
+              Tensorflow, opencv, Scikit-learn ,Numpy, Pandas, Seaborn, Matplotlib
+              Supervised and unsupervised learning, statistical modeling  
+              Git
+              Hadoop
+              IBM SPSS, Tableau
+              Domain knowledge - Banking.
+              Can speak English, Tamil and Hindi fluently.
+              
               </p>
             </div>
           </div>
@@ -143,16 +147,16 @@ export function Body() {
           <h1>Education</h1>
           <div className="p-5">
             <h2>Vellore Institute of Technology</h2>
-            <h4>Bachelor of Technology - BTech, Computer Science with spl Data Science</h4>
+            <h4>Bachelor of Technology - BTech, Computer Science engineering core</h4>
             <h4>Grade: 8.93 CGPA</h4>
             <h4>Vellore, Tamil Nadu, India(On-site)</h4>
           </div>
 
           <div className="p-5">
-            <h2>The Amaatra Academy, Bangalore</h2>
+            <h2>Tchettinad vidyashram, chennai</h2>
             <h4>12th Grade CBSE, PCMC</h4>
-            <h4>Grade: 95.6%</h4>
-            <h4>Bangalore, Karnataka, India(On-site)</h4>
+            <h4>Grade: 98.0%</h4>
+            <h4>Chennai,Tamilnadu India(On-site)</h4>
           </div>
         </div>
       </div>
